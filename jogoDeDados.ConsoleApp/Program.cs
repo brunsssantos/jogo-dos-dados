c7f923af7ea91beb8f02ea141e847d8eb9af4c74
Bruna Santos <brunasan7os@gmail.com>
2025-04-01 19:48:52 -0300

    Implementado estrutura básica do sistema

--- jogoDeDados.ConsoleApp/Program.cs
+++ jogoDeDados.ConsoleApp/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+           
+                ExibirCabecalho();
+
+                int resultado = LancarDado();
+
+                ExibirResultadoSorteio(resultado);
+
+                string opcaoContinuar = ExibirMenuContinuar();
+                
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+
+            static void ExibirCabecalho()
+            {
+                Console.Clear();
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Jogo dos Dados");
+                Console.WriteLine("-----------------------------------------");
+
+                Console.Write("Pressione ENTER para lançar o dado...");
+                Console.ReadLine();
+            }
+
+            static int LancarDado()
+            {
+                Random geradorDeNumeros = new Random();
+
+                int resultado = geradorDeNumeros.Next(1, 7);
+
+                return resultado;
+            }
+
+            static void ExibirResultadoSorteio(int resultado)
+            {
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine($"O valor sorteado foi: {resultado}");
+                Console.WriteLine("-----------------------------------------");
+
+             
+            }
+
+            static string ExibirMenuContinuar()
+            {
+                Console.WriteLine("Deseja continuar? (S/N ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+                return opcaoContinuar;
+            }
         }
     }
 }
