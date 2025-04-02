e1eca950bb222bec082dd8e8bb60c55dfa1b06a2
Bruna Santos <brunasan7os@gmail.com>
2025-04-01 21:40:04 -0300

    Implementado o controle da posição do jogador

--- jogoDeDados.ConsoleApp/Program.cs
+++ jogoDeDados.ConsoleApp/Program.cs
@@ -1,61 +1,87 @@
-﻿namespace jogoDeDados.ConsoleApp
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace jogoDeDados.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            const int limiteLinhaChegada = 30;
+
             while (true)
             {
-           
-                ExibirCabecalho();
+                int posicaoUsuario = 0;
+                bool jogoEstaEmAndamento = true;
 
-                int resultado = LancarDado();
+                while (jogoEstaEmAndamento)
+                {
+                    ExibirCabecalho();
 
-                ExibirResultadoSorteio(resultado);
+                    int resultado = LancarDado();
+
+                    ExibirResultadoSorteio(resultado);
+
+                    posicaoUsuario += resultado;
+
+                    if (posicaoUsuario >= limiteLinhaChegada)
+                    {
+                        Console.WriteLine("Parabéns, você alcançou a linha de chegada!");
+
+                        jogoEstaEmAndamento = false;
+                    }
+                    else
+                        Console.WriteLine($"O jogador está na posição: {posicaoUsuario} de {limiteLinhaChegada}");
+
+                    Console.WriteLine("Pressione ENTER para continuar...");
+                    Console.ReadLine();
+                }
 
                 string opcaoContinuar = ExibirMenuContinuar();
-                
+
 
                 if (opcaoContinuar != "S")
                     break;
             }
+        }
 
-            static void ExibirCabecalho()
-            {
-                Console.Clear();
-                Console.WriteLine("-----------------------------------------");
-                Console.WriteLine("Jogo dos Dados");
-                Console.WriteLine("-----------------------------------------");
+        static void ExibirCabecalho()
+        {
+            Console.Clear();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Jogo dos Dados");
+            Console.WriteLine("-----------------------------------------");
 
-                Console.Write("Pressione ENTER para lançar o dado...");
-                Console.ReadLine();
-            }
+            Console.Write("Pressione ENTER para lançar o dado...");
+            Console.ReadLine();
+        }
 
-            static int LancarDado()
-            {
-                Random geradorDeNumeros = new Random();
+        static int LancarDado()
+        {
+            Random geradorDeNumeros = new Random();
 
-                int resultado = geradorDeNumeros.Next(1, 7);
+            int resultado = geradorDeNumeros.Next(1, 7);
 
-                return resultado;
-            }
+            return resultado;
+        }
 
-            static void ExibirResultadoSorteio(int resultado)
-            {
-                Console.WriteLine("-----------------------------------------");
-                Console.WriteLine($"O valor sorteado foi: {resultado}");
-                Console.WriteLine("-----------------------------------------");
+        static void ExibirResultadoSorteio(int resultado)
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine($"O valor sorteado foi: {resultado}");
+            Console.WriteLine("-----------------------------------------");
 
              
-            }
+        }
 
-            static string ExibirMenuContinuar()
-            {
-                Console.WriteLine("Deseja continuar? (S/N ");
-                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+        static string ExibirMenuContinuar()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Deseja continuar? (S/N) ");
+            string opcaoContinuar = Console.ReadLine()!.ToUpper();
 
-                return opcaoContinuar;
-            }
+            return opcaoContinuar;
         }
     }
 }
